eb4a391d2d57e146c2dfd7096e19ecd817a30168
THe-L00t <93660724+THe-L00t@users.noreply.github.com>
2025-09-25 20:40:30 +0900

    윈도우 생성 / glew 초기화

--- main.cpp
+++ main.cpp
@@ -3,3 +3,20 @@
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
 
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("TestEngine made by THe-L00t");
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) { //--- glew √ ±‚»≠ 
+		std::cerr << "Unable to initialize GLEW" << std::endl
+			;
+		exit(EXIT_FAILURE);
+	}
+	else {
+		std::cout << "GLEW Initialized\n";
+	}
+}
